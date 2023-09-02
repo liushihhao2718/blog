cd125095220f4da0c91c85f928a7a740f62ef53c
Tom1_Liu <tom1_liu@asus.com>
2023-09-02 10:07:15 +0800

    remove parcel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-// vite.config.js
 /** @type {import('vite').UserConfig} */
 export default {
   base:"/",
-  assetsInclude: ['**/*.md']
+  root:'./clean-blog',
+  assetsInclude: ['**/*.md'],
+  build:{
+    emptyOutDir: true,
+    oututDir:'../docs',
+    rollupOptions: {
+      input: {
+        main: './clean-blog/index.html',
+        about: './clean-blog/about.html',
+        'Why-I-can-t-get-a-girl-friend':'./clean-blog/Why-I-can-t-get-a-girl-friend/Why-I-can-t-get-a-girl-friend.html'
+      }
+    }
+  }
 }
